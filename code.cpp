be196f1d96a888426259025543a118955a1ad1ef
23AIML025 <166367968+harshzz01@users.noreply.github.com>
2024-04-11 13:05:20 +0530

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -53,3 +53,23 @@ cin>>admission_year[i];
 total=ch+total; //making choice
 }
 }
+void show()
+{
+if(total==0)
+{
+cout<<"No Data is Entered"<<endl;
+}
+else
+{
+for(int i=0;i<total;i++)
+{
+cout<<"\nData of Student "<<i+1<<endl<<endl;
+cout<<"Roll NO "<<roll_no[i]<<endl;
+cout<<"Name "<<name[i]<<endl;
+cout<<"Class "<<Class[i]<<endl;
+cout<<"Course "<<course[i]<<endl;
+cout<<"Mobile NO "<<mobile_no[i]<<endl;
+cout<<"Admission Year "<<admission_year[i]<<endl;
+}
+}
+}
