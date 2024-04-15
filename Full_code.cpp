0c6933222b69a0cbab6bb9131ef56b677eaf463f
23AIML025 <166367968+harshzz01@users.noreply.github.com>
2024-04-15 12:23:01 +0530

    Full_code.cpp

--- Full_code.cpp
+++ Full_code.cpp
@@ -98,3 +98,42 @@ cout<<"Admission Year "<<admission_year[i]<<endl;
 }
 }
 }
+void update()
+{
+if(total==0)
+{
+cout<<"No data is entered"<<endl;
+}
+else{
+string rollno;
+cout<<"Enter the roll no of student which you want to update"<<endl;
+cin>>rollno;
+for(int i=0;i<total;i++)
+{
+if(rollno==roll_no[i])
+{
+cout<<"\nPrevious data"<<endl<<endl;
+cout<<"Data of Student "<<i+1<<endl;
+cout<<"Roll NO: "<<roll_no[i]<<endl;
+cout<<"Name "<<name[i]<<endl;
+cout<<"Class "<<Class[i]<<endl;
+cout<<"Course "<<course[i]<<endl;
+cout<<"Mobile NO "<<mobile_no[i]<<endl;
+cout<<"Admission Year "<<admission_year[i]<<endl;
+cout<<"\nEnter new data"<<endl<<endl;
+cout<<"Enter Roll NO ";
+cin>>roll_no[i];
+cout<<"Enter Name: ";
+cin>>name[i];
+cout<<"Enter Class: ";
+cin>>Class[i];
+cout<<"Enter Course: ";
+cin>>course[i];
+cout<<"Enter Mobile NO: ";
+cin>>mobile_no[i];
+cout<<"Enter Admission Year: ";
+cin>>admission_year[i];
+}
+}
+}
+}
