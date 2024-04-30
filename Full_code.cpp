737daa05a1cf77a318ba38a82767b146791135b7
23AIML025 <166367968+harshzz01@users.noreply.github.com>
2024-04-30 19:28:41 +0530

    Full_code.cpp

--- Full_code.cpp
+++ Full_code.cpp
@@ -137,3 +137,64 @@ cin>>admission_year[i];
 }
 }
 }
+void Delete()
+{
+if(total==0)
+{
+cout<<"No data is entered yet"<<endl;
+}
+else
+{
+int a;
+cout<<"Are you Sure to Delete Data?"<<endl;
+cout<<"Press 1 to delete all record"<<endl;
+cin>>a;
+if(a==1)
+{
+total=0;
+cout<<"All record is deleted..!!"<<endl;
+}
+else
+{
+cout<<"Please Press 1 to Delete All Record"<<endl;
+}
+}
+}
+int main() //main function, execution of program starts from here
+{
+int value;
+while(true) //always in Loop
+{
+cout<<"\nPress 1 to Enter data"<<endl<<endl;
+cout<<"Press 2 to Show data"<<endl<<endl;
+cout<<"Press 3 to Search data"<<endl<<endl;
+cout<<"Press 4 to Update data"<<endl<<endl;
+cout<<"Press 5 to Delete data"<<endl<<endl;
+cout<<"Press 6 to Quit"<<endl<<endl;
+cin>>value;
+switch(value)
+{
+case 1:
+enter();
+break;
+case 2:
+show();
+break;
+case 3:
+search();
+break;
+case 4:
+update();
+break;
+case 5:
+Delete();
+break;
+case 6:
+exit(0);
+break;
+default:
+cout<<"Invalid input"<<endl;
+break;
+}
+}
+}
